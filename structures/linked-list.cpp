cc036dfbc697ca98b618891c61334a1bcd8dc078
Programmer-Adithya-Iyer <work.adithyaiyer@gmail.com>
2025-10-08 16:58:35 +0000

    Finalized pop in linked list

--- structures/linked-list.cpp
+++ structures/linked-list.cpp
@@ -65,16 +65,40 @@ Node* put(Node* head, int x, int index = 0) {
     return head;
 }
 
-// Popping from the linked list
+// Popping from the linked list at a given index
 Node* pop(Node* head, int index) {
-	Node* currentNode = head;
-	int counter = 0;
-	while (counter < (index - 1) && currentNode) {
-		currentNode = currentNode->next;
-	}
-	delete currentNode->next;
-	currentNode->next = NULL;
-	return head;
+    if (head == nullptr) {
+        std::cout << "List is empty. Nothing to pop." << std::endl;
+        return head;
+    }
+
+    // If removing the head node
+    if (index == 0) {
+        Node* newHead = head->next;
+        delete head;
+        return newHead;
+    }
+
+    Node* currentNode = head;
+    int counter = 0;
+
+    // Move to node just before the one to delete
+    while (counter < index - 1 && currentNode->next != nullptr) {
+        currentNode = currentNode->next;
+        counter++;
+    }
+
+    // If index is out of range
+    if (currentNode->next == nullptr) {
+        std::cout << "Index out of range. Pop failed." << std::endl;
+        return head;
+    }
+
+    Node* nodeToDelete = currentNode->next;
+    currentNode->next = nodeToDelete->next;
+    delete nodeToDelete;
+
+    return head;
 }
 
 // Display the linked list
