637395786cc46957129853c92b4149c6a5c6d556
Programmer-Adithya-Iyer <work.adithyaiyer@gmail.com>
2025-10-10 08:42:34 +0000

    Added push function
    Doubly linked list

--- structures/doubly-linked-list.cpp
+++ structures/doubly-linked-list.cpp
@@ -1,11 +1,35 @@
 #include <iostream>
 
+// Node for Doubly Linked List
 class Node {
 public:
 	int value;
 	Node* prev;
 	Node* next;
 };
+
+// Pushing to Doubly Linked List
+Node* push (Node* head, int x) {
+	Node* newNode = new Node();
+	Node* currentNode = head;
+
+	newNode->value = x;
+	
+	if (head == nullptr) {
+		newNode->next = newNode->prev = nullptr;
+		return newNode;
+	} 
+
+	while(currentNode->next != nullptr) {
+		currentNode = currentNode->next;
+	}
+
+	currentNode->next = newNode;
+	newNode->prev = currentNode;
+	newNode->next = nullptr;
+
+	return head;
+}
 
 int main() {
 	Node* head = nullptr;
